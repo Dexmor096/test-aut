876f912026ea69fcd2ce7c005b79b81e1785ba81
Dexter096 <gr1nch2@yandex.ru>
2024-02-04 14:41:54 +0300

    feat: login add

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,9 +1,15 @@
 // import userService from '../service/user-service.js';
-const userService = require('../service/user-service.js')
+const userService = require('../service/user-service.js');
+const {validationResult} = require('express-validator');
+const ApiError = require('../exceptions/api-error')
 
 class userController {
 	async registration (req, res, next) {
 		try{
+			const errors = validationResult(req);
+			if(!errors.isEmpty()) {
+				return next(ApiError.BadRequest("Ошибка при валидации", errors.array()))
+			}
 			const { email, password } = req.body;
 			const userData = await userService.registration(email, password);
 			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
@@ -14,7 +20,10 @@ class userController {
 	}
 	async login (req, res, next) {
 		try{
-
+			const { email, password } = req.body;
+			const userData = await userService.login(email, password);
+			res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+			return res.json(userData)
 		} catch (e) {
 			next(e)
 		}
--- server/router/index.js
+++ server/router/index.js
@@ -1,11 +1,15 @@
 // import { Router } from 'express';
 // import userController from '../controllers/user-controller.js';
 const Router = require('express').Router;
-const userController = require('../controllers/user-controller.js')
+const userController = require('../controllers/user-controller.js');
+const {body} = require('express-validator')
 
 const router = new Router()
 
-router.post('/registration', userController.registration);
+router.post('/registration',
+    body('email').isEmail(),
+    body('password').isLength({min: 2, max: 32}),
+    userController.registration);
 router.post('/login', userController.login);
 router.post('/logout', userController.logout);
 router.get('/activate/:link', userController.activate);
