d73be57673b08a6c82b7495dc35327a069224314
Dexter096 <gr1nch2@yandex.ru>
2024-02-04 18:50:17 +0300

    fix: token validation

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -1,7 +1,7 @@
 const ApiError = require('../exceptions/api-error');
 const tokenService = require('../service/token-service')
 
-module.exports = function (req, res, next) {
+module.exports = async function (req, res, next) {
     try {
         const authorizationHeader = req.headers.authorization;
         if(!authorizationHeader) {
@@ -9,9 +9,10 @@ module.exports = function (req, res, next) {
         }
         const accessToken = authorizationHeader.split(' ')[1];
         if(!accessToken) {
+            console.log(accessToken)
             return next(ApiError.UnauthorizedError())
         }
-        const userData = tokenService.validateAccessToken(accessToken);
+        const userData = await tokenService.validateAccessToken(accessToken);
         if(!userData) {
             return next(ApiError.UnauthorizedError())
         }
